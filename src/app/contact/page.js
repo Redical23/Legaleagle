036b3efc4c899500659b59f787b100dff9af32c6
Redical23 <gfhkhihhi@gmail.com>
2025-02-25 21:52:04 +0530

    version1.04.1

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      You may contact us using the information below: Merchant Legal entity name: Rishi Gupta Registered Address: 7 old lashkar line Allahabad UTTAR PRADESH 211003 Telephone No: 9305371779 E-Mail ID: Properaid45@gmail.com
+    </div>
+  )
+}
+
+export default page
